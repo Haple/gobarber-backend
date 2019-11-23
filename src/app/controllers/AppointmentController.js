0ed091f8515a3005dced982643c8e53d5790e392
Aleph Santos Oliveira <aleph.oliveira@etec.sp.gov.br>
2019-11-23 20:39:24 -0300

    Aplicando paginação

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -6,10 +6,18 @@ import File from '../models/File';
 
 class AppointmentController {
   async index(req, res) {
+    const { page = 1 } = req.query;
+    if (page <= 0) {
+      return res.status(400).json({
+        error: 'Validation fails',
+      });
+    }
     const appointments = await Appointment.findAll({
       where: { user_id: req.userId, canceled_at: null },
       order: ['date'],
       attributes: ['id', 'date'],
+      limit: 20,
+      offset: (page - 1) * 20,
       include: [
         {
           model: User,
