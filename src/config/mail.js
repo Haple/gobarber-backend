190797a91b04593d3db66734cb63e3ad4ff7dec5
Aleph Santos Oliveira <aleph.oliveira@etec.sp.gov.br>
2019-11-28 15:32:01 -0300

    Configuração das variáveis de ambiente
    Signed-off-by: Aleph Santos Oliveira <aleph.oliveira@etec.sp.gov.br>

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: 2525,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: '030c078bf7503b',
-    pass: '5251a4adb2a9f4',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Equipe GoBarber <noreply@gobarber.com>',
--- src/config/sentry.js
+++ src/config/sentry.js
@@ -1,3 +1,3 @@
 export default {
-  dsn: 'https://5fe16c3ea52342c3817b3031fdb6aa52@sentry.io/1837955',
+  dsn: process.env.SENTRY_DSN,
 };
